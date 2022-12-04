ba32dbf4565894bd8be0466a035411379258ef8b
dpc2 <dpculler91@gmail.com>
2022-12-04 08:12:20 -0700

    Python Problem Solving, adding Ch.1 files

---
+++ python/problemSolving/ch1/1.4.1.c_tuples.py
@@ -0,0 +1,11 @@
+# 1.4.1.c - Tuples
+
+myTuple = (2,True,3.14)
+print(myTuple)
+print(len(myTuple))
+print(myTuple[0])
+print(myTuple * 3)
+print(myTuple[0:2])
+
+# Immutability
+myTuple[1] = False
---
+++ python/problemSolving/ch1/1.4.1.d_sets.py
@@ -0,0 +1,16 @@
+# 1.4.1.d - Sets
+
+print({3,6,"Nugget",4.5,True})
+mySet = {3,6,"Nugget",4.5,True}
+print(mySet)
+
+# Operations on a set
+print(len(mySet))
+print(True in mySet)
+print("Mochi" in mySet)
+
+# Methods on a set
+mySet = {False, 3, 4.5, 6, 'Nugget'}
+yourSet = {99, 3, 100}
+
+print(mySet.union(yourSet))
