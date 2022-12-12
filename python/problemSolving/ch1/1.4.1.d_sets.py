2541c964cc79213aca3114bfc153b0687ef72a8e
dpc2 <dpculler91@gmail.com>
2022-12-11 21:59:25 -0700

    More collection data types

--- python/problemSolving/ch1/1.4.1.d_sets.py
+++ python/problemSolving/ch1/1.4.1.d_sets.py
@@ -14,3 +14,18 @@ mySet = {False, 3, 4.5, 6, 'Nugget'}
 yourSet = {99, 3, 100}
 
 print(mySet.union(yourSet))
+print(mySet | yourSet)
+print(mySet.intersection(yourSet))
+print(mySet & yourSet)
+print(mySet.difference(yourSet))
+print(mySet - yourSet)
+print(mySet.issubset(yourSet))
+print({3,100} <= yourSet)
+mySet.add("house")
+print(mySet)
+mySet.remove(4.5)
+print(mySet)
+mySet.pop()
+print(mySet)
+mySet.clear()
+print(mySet)
