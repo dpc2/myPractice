216041e0a17eedb9d8586aa052819d23879e5f10
dpc2 <dpculler91@gmail.com>
2023-01-16 12:24:56 -0700

    Chapter 22 examples

--- python/selfTaught/ch22/ch22.py
+++ python/selfTaught/ch22/ch22.py
@@ -2,7 +2,9 @@
 #                   Algorithms                   #
 #------------------------------------------------#
 
-# Fizzbuzzzz
+#------------------------------------------------#
+#                    Fizzbuzz                    #
+#------------------------------------------------#
 
 def myFizzbuzz():
     for i in range(1,101):
@@ -19,7 +21,9 @@ myFizzbuzz()
 print()
 
 
-# Sequential Search
+#------------------------------------------------#
+#                Sequential Search               #
+#------------------------------------------------#
 
 # A search algorithm finds information in a
 # data structure like a list.
@@ -45,7 +49,9 @@ print(search2)
 print()
 
 
-# Palindrome
+#------------------------------------------------#
+#                   Palindrome                   #
+#------------------------------------------------#
 
 def palindrome(word):
     word = word.lower()
@@ -53,7 +59,84 @@ def palindrome(word):
 
 print(palindrome("Mother"))
 print(palindrome("Atoyota"))
+print()
 
 
 
-# Anagram
+#------------------------------------------------#
+#                    Anagram                     #
+#------------------------------------------------#
+
+def anagram(w1, w2):
+    w1 = w1.lower()
+    w2 = w2.lower()
+    return sorted(w1) == sorted(w2)
+
+print(anagram('iceman', 'cinema'))
+print(anagram('leaf', 'tree'))
+print()
+
+
+
+#------------------------------------------------#
+#                 Count characters               #
+#------------------------------------------------#
+
+def count_characters(string):
+    count_dict = {}
+    for c in string:
+        if c in count_dict:
+            count_dict[c] += 1
+        else: count_dict[c] = 1
+    print(count_dict)
+
+count_characters('Dynasty')
+
+
+
+#------------------------------------------------#
+#                    Recursion                   #
+#------------------------------------------------#
+
+# Recursion is solving problems by breaking them up
+# into smaller problems that are more easily solved.
+
+# So far we've used iterative algorithms, repeating
+# steps over and over using a loop.
+# Recursive algorithms rely on functions that call
+# themselves, which can be a more elegant solution.
+
+# A recursive algorithm is defined inside of a function.
+# The function must have a base case, a condition to
+# stop it from continuing forever.
+
+# Each time the function calls itself, it moves closer
+# to the base case, which is eventually satisfied and
+# the function stops calling itself.
+
+# 3 laws of recursion:
+# - Recursive algorithm must have a base case
+# - It must change its state and move closer to the
+#   base case
+# - It must call itself, recursively.
+
+
+def bottles_of_beer(bob):
+    """ Prints 99 Bottles of Beer
+    on the Wall lyrics.
+    :param bob: Must be a positive
+    integer    
+    """
+    if bob < 1:
+        print("No more bottles of beer on the wall. "
+        "No more bottles of beer.")
+        return
+    tmp = bob
+    bob -= 1
+    print("{} bottles of beer on the wall, "
+    "{} bottles of beer, you take one down, "
+    "pass it around, {} bottles of beer on the "
+    "wall.".format(tmp, tmp, bob))
+    bottles_of_beer(bob)
+
+bottles_of_beer(99)
