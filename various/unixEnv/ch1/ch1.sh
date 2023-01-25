9feb6bfbabbd7d6e30c857a36b585f84f3464de6
dpc2 <dpculler91@gmail.com>
2023-01-24 22:50:55 -0700

    Chapter 1 contributions

---
+++ various/unixEnv/ch1/ch1.sh
@@ -0,0 +1,28 @@
+# Bash commands
+
+ls -lut
+ls -lrt
+
+pr junk temp
+
+
+# cat - named file(s) are "catenated"
+# onto the terminal
+
+cat junk temp
+
+
+
+# ed - old screen editor
+
+ed
+a
+Hullo World!
+.
+w junk
+q
+
+ed junk
+19
+1,$p	# Print lines 1 through last
+
---
+++ various/unixEnv/ch1/loop.py
@@ -0,0 +1,6 @@
+from time import sleep
+
+while 1:
+	print("Hello! This is a test.")
+	sleep(5)
+	print("Hello! Still testing...")
