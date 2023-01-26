aab8ecd96a25fa655d1b266bc3132ee30739476b
dpc2 <dpculler91@gmail.com>
2023-01-25 22:19:32 -0700

    More chapter 1 work

--- various/unixEnv/ch1/ch1.sh
+++ various/unixEnv/ch1/ch1.sh
@@ -1,9 +1,12 @@
+# 1.2 - Day to day use: files and common commands
+
 # Bash commands
 
 ls -lut
 ls -lrt
 
-pr junk temp
+# print files in an 8.5x11 format
+pr -2 junk temp
 
 
 # cat - named file(s) are "catenated"
@@ -26,3 +29,29 @@ ed junk
 19
 1,$p	# Print lines 1 through last
 
+
+# word count
+
+# 8 lines, 46 words, 255 characters
+wc poem
+
+
+# grep - g/regular-expression/p
+# v - invert
+
+grep fleas poem
+grep -v fleas poem
+
+
+# sort
+sort poem
+
+sort -r     # Reverse order
+sort -n     # Numeric order
+sort -nr    # Reverse numeric
+sort -f     # Fold upper, lower case
+sort +n     # Sort starting at n+1
+
+
+# tail - prints last 10 lines
+tail -1 poem
