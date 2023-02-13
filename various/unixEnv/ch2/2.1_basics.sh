8748b848dbf9e657c38bf109670927999f5e33cc
dpc2 <dpculler91@gmail.com>
2023-02-13 00:19:42 -0700

    Starting chapter 2

---
+++ various/unixEnv/ch2/2.1_basics.sh
@@ -0,0 +1,15 @@
+#------------------------------------------------#
+#              2.1 - Basics of Files             #
+#------------------------------------------------#
+
+ed
+.
+a
+now is the time
+for all good people
+.
+w junk
+q
+
+# od - octal dump: visible representation of all bytes
+od -c junk
