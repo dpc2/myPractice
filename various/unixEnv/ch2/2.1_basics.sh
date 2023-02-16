0971cec231771b2f1bc8d3f4bd9cb54e4b498788
dpc2 <dpculler91@gmail.com>
2023-02-15 23:40:17 -0700

    More beginning chapter 2: file basics

--- various/unixEnv/ch2/2.1_basics.sh
+++ various/unixEnv/ch2/2.1_basics.sh
@@ -12,4 +12,12 @@ w junk
 q
 
 # od - octal dump: visible representation of all bytes
+# c - 'interpret bytes as characters'
+# b - octal, base 8? <-- Hold over from PDP-11!
+# x - hex
 od -c junk
+
+
+# cat -u: unbuffered
+# cat is unbuffered by default now
+cat -u
