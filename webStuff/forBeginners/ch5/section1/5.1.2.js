e5bfb678374ebe509a1d861c545590f26a963e27
dpc2 <dpculler91@gmail.com>
2023-02-26 20:15:46 -0700

    Starting chapter 5: JavaScript DOM - intro, DOM manipulation

---
+++ webStuff/forBeginners/ch5/section1/5.1.2.js
@@ -0,0 +1,8 @@
+var el1 = document.querySelector('h1');
+console.log(el1);
+console.dir(el1.innerHTML);
+
+el1.innerHTML = "Hello World";
+el1.style.color = 'red';
+el1.style.background = 'blue';
+el1.innerText = "TEST";
