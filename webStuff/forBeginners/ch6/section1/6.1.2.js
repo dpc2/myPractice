ce4a1aee8b782c38e0e21ccf645cae421939eb14
dpc2 <dpculler91@gmail.com>
2023-03-09 22:37:53 -0700

    Chapter 6: JSON as data

---
+++ webStuff/forBeginners/ch6/section1/6.1.2.js
@@ -0,0 +1,6 @@
+var myNumber = {"name": 10}
+var myString = {"name": "Hello, World!"}
+var myBoolean = {"name": true}
+var myArray = {"name": [{"name1": 1}, "hello", "world"]}
+var myObject = {"name": {"name1": 1, "name2": 1}}
+var myNull = {"name": null}
