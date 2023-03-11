de0f2e48b032659e6ab05d6a561d9a22f936a93b
dpc2 <dpculler91@gmail.com>
2023-03-10 23:40:23 -0700

    Chapter 2 work: permissions

---
+++ various/unixEnv/ch2/2.3.5_exercises.sh
@@ -0,0 +1,10 @@
+# Exercise 2.2 - ls operation
+cat . >foo
+ls -f foo
+
+# Exercise 2.3 - pwd operation?
+
+
+# Exercise 2.4 - find vs du
+time du -a | grep jb_lldb_declarative_formatters_options
+time find . -name "jb_lldb_declarative_formatters_options"
---
+++ various/unixEnv/ch2/2.4_permissions.sh
@@ -0,0 +1,13 @@
+#------------------------------------------------#
+#                2.4 - Permissions               #
+#------------------------------------------------#
+
+# super user
+su
+
+# encrypt a file
+# crypt xxx
+mcrypt xxx
+
+# Users are checked against the password file
+grep danny /etc/passwd
