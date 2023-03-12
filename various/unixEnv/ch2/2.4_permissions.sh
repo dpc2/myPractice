8d3e35e43c2b523c3c0a8ea8a40bfcd936621825
dpc2 <dpculler91@gmail.com>
2023-03-11 21:52:36 -0700

    More chapter 2: permissions

--- various/unixEnv/ch2/2.4_permissions.sh
+++ various/unixEnv/ch2/2.4_permissions.sh
@@ -9,5 +9,35 @@ su
 # crypt xxx
 mcrypt xxx
 
-# Users are checked against the password file
+# Check users against the password file
 grep danny /etc/passwd
+
+# ls -l gives more information
+ls -l /etc/passwd
+
+# /etc/group has group information
+ls -l /etc/group
+
+# newgrp changes group permissions
+newgrp
+
+
+# password can be changed with /bin/passwd
+ls -l /bin/passwd
+
+# --> -rwsr-xr-x 1 root root 63960 Feb  7  2020 /bin/passwd
+# passwd is a "set-uid" program, it uses the set-uid bit.
+# If its permissions were -rwsrwxrwx, any user could modify
+# it to do something dangerous.
+
+
+# /bin/who is a bit more ordinary
+ls -l /bin/who
+
+
+# Diretory permissions work similarly, -d option asks for directory info
+ls -ld .
+
+# You cannot write to a directory
+who >.
+# bash: .: Is a directory
