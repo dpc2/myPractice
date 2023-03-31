a890077144166ea1ec62560ac8003211c361658f
dpc2 <dpculler91@gmail.com>
2023-03-30 22:55:25 -0600

    Chapter 2.5: Exercises 2-6 and 2-7

---
+++ various/unixEnv/ch2/2.5.5_exercises.sh
@@ -0,0 +1,24 @@
+# Exercise 2-6
+# Why does ls -li show 4 links for recipes?
+# Hint: Try 'ls -ld /usr/you'
+ls -li
+
+# 4 links because recipes have 2 folders,
+# 2 files in it
+
+
+
+# Exercise 2-7
+# Difference between:
+mv junk junk1
+
+# and
+cp junk junk1
+rm junk
+
+# cp results in a file with the same contents,
+# but different i-node
+
+
+
+# Exercise 2-8
