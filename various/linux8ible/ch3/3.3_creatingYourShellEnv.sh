87f3a01f065055911fae37d08847ac21f2d4ede4
Danny C <dpculler91@gmail.com>
2023-06-25 20:18:23 -0600

    Chapter 3: Creating your shell env

---
+++ various/linux8ible/ch3/3.3_creatingYourShellEnv.sh
@@ -0,0 +1,18 @@
+#------------------------------------------#
+#   3.3 - Creating Your Shell Environment  #
+#------------------------------------------#
+
+#--- Configuring your shell ---#
+
+# System-wide (sudo required):
+/etc/profile
+/etc/bashrc
+# preferred method:
+/etc/profile.d/custom.sh
+
+# Individual user settings
+~/.bash_profile
+~/.bashrc
+~/.bash_logout
+
+1991
