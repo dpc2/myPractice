f2871e9e83a7c5ac78a7af8eb845b926fc0e53c9
Danny C <dpculler91@gmail.com>
2023-07-08 23:58:51 -0600

    Starting Chapter 5

---
+++ various/linux8ible/ch5/5.1_editingFilesVimVi.sh
@@ -0,0 +1,58 @@
+#------------------------------------------#
+#    5.1 - Editing Files with vim and vi   #
+#------------------------------------------#
+
+# Alternatives
+gedit
+nano
+jed
+joe
+kate
+kedit
+mcedit
+nedit
+
+
+#--- Starting with vi ---#
+vi /tmp/test
+
+# Adding text:
+# a: add
+# A: add at end
+# i: insert
+# I: insert at beginning
+# o: open below
+# O: open above
+
+# Moving around in the text:
+# Arrow keys
+# w: beginning of next word
+# W: beginning of next word
+# b: beginning of previous word
+# B: beginning of previous word
+# 0: beginning of current line
+# $: end of current line
+# H: upper-left corner of screen
+# M: first character of middle line
+# L: lower-left corner of screen
+
+# Deleting, copying, and changing text:
+# ...
+# Blablabla
+
+vimtutor
+
+
+
+#--- Finding Files ---#
+
+# Find commands by name
+locate xxx
+# Find files based on attributes
+find xxx
+# Search within text files
+grep xxx
+
+
+# Using locate to find files by name
+
