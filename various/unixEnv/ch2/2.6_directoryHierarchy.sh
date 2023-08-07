e4b127f4954eb1e2b195ec8e3294e644956a8b72
dpc2 <dpculler91@gmail.com>
2023-08-06 23:04:07 -0600

    Moving test files around

--- various/unixEnv/ch2/2.6_directoryHierarchy.sh
+++ various/unixEnv/ch2/2.6_directoryHierarchy.sh
@@ -7,3 +7,12 @@ ls /
 # /unix folder no longer relevant?
 # At boot: /boot is read in, followed by /unix
 
+# File system hierarchy:
+# /bin - (binaries) Basic programs like who and ed live here
+# /dev - devices
+# /etc - (et cetera) Contains various admin files and sytem files
+# /lib - (library) Contains primarily parts of the C compiler,
+#	preprocessor, subroutine library
+# /tmp - (temporaries) Contains short-lived files
+# /usr - (user file system) Has its own bin, lib, sbin, src directories
+
