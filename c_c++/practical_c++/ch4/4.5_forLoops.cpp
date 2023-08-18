f8611260b071d49174c1f15e7653552d53971b0c
dpc2 <dpculler91@gmail.com>
2023-08-17 22:49:28 -0600

    Ch. 4: for and while loops

--- c_c++/practical_c++/ch4/4.5_forLoops.cpp
+++ c_c++/practical_c++/ch4/4.5_forLoops.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main(){
 
+    int myMax = 1000;
+
+    for (int i=0; i< myMax; i++){
+        cout << "Hello World! " + to_string(i) << endl;
+        cout << "Hello! " << i << endl;
+    }
+
+    
 
     return 0;
 }
---
+++ c_c++/practical_c++/ch4/4.6_whileLoops.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    // for (int i=0; i< 100; i++){
+    //     cout << "Hello! " << i << endl;
+    // }
+
+    int i = 0;
+    while (i < 10){
+        cout << "Hellooo " << i << endl;
+        i++;
+    }
+    
+
+    return 0;
+}
