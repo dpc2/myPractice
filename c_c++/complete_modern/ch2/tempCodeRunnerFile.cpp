215a7d876e386482505585c1d9a649d7cc31e53d
dpc2 <dpculler91@gmail.com>
2023-09-08 18:42:26 -0600

    Cleaning up

---
+++ c_c++/complete_modern/ch2/tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+    // int age;
+    // cout << "How old are you?\n";
+    // cin >> age;
+    // cout << "Your age is: " << age << endl;
+
+
+    // // Read a string from the keyboard
+    // char buffer[1000];
+    // cout << "What would like you like to say?\n";
+    // // cin >> buffer;
+    // cin.getline(buffer, 1000, '\n');
+    // cout << "You said: " << buffer << endl;
