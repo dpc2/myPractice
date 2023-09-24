ff8205129515c73ea6c87eff2c4362c556f93a91
Daniel C <dpculler91@gmail.com>
2023-09-23 22:52:47 -0700

    Starting C# course

---
+++ c_c++_c#/c#/basicsForAbsoluteBeginners/ch1/1.2_basics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Basics
+{
+    class Program
+    {
+        /// <summary>
+        /// Basics of the C# language
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+
+            // C# Blocks
+            if(true)
+            {
+                int a = 100;
+                int b = a;
+            }
+
+            // C# Comments
+            // Single line comments
+            /* Multi
+            line comments */
+            /// Documentation comments
+
+            // C# Read and Write
+            string str = Console.ReadLine();
+            Console.WriteLine("Hello World!");
+            Console.Write("hello");
+
+            Console.WriteLine(str);
+        }
+    }
+}
