1efc6051ebf4667ff9bb1a9f8590a600508988fa
Danny C <dpculler91@gmail.com>
2023-10-01 20:55:10 -0600

    Starting Ch6: Managing Running Processes

---
+++ various/linux8ible/ch6/6.1_listing_processes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#------------------------------------------#
+#      	  6.1 - Listing Processes	   #
+#------------------------------------------#
+#
+# Listing processes with ps
+# u = "username"
+ps u
+
+# View all running processes
+ps ux | less
+
+# View all processes for all users
+ps aux | less
+
